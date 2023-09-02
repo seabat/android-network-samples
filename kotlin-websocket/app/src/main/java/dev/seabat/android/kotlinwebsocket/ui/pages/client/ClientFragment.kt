988972adaca951864c7d7693780ebf38db6bb523
seabat <ryouta.nakasnishi.86@gmail.com>
2023-09-03 00:09:38 +0900

    レイアウトを初期化

--- kotlin-websocket/app/src/main/java/dev/seabat/android/kotlinwebsocket/ui/pages/client/ClientFragment.kt
+++ kotlin-websocket/app/src/main/java/dev/seabat/android/kotlinwebsocket/ui/pages/client/ClientFragment.kt
@@ -2,33 +2,25 @@ package dev.seabat.android.kotlinwebsocket.ui.pages.client
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import dev.seabat.android.kotlinwebsocket.R
-import dev.seabat.android.kotlinwebsocket.databinding.PageQiitaBinding
-import dev.seabat.android.kotlinwebsocket.ui.dialog.showSimpleErrorDialog
-import dev.seabat.android.kotlinwebsocket.utils.convertToJapaneseCalender
-import dev.seabat.android.kotlinwebsocket.utils.getDateFromBundle
+import dev.seabat.android.kotlinwebsocket.databinding.PageClientBinding
 import java.util.Date
 
 @AndroidEntryPoint
-class ClientFragment : Fragment(R.layout.page_qiita) {
+class ClientFragment : Fragment(R.layout.page_client) {
     companion object {
         val TAG: String = ClientFragment::class.java.simpleName
     }
 
-    private var binding: PageQiitaBinding? = null
+    private var binding: PageClientBinding? = null
     private val viewModel: ClientViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = PageQiitaBinding.bind(view)
+        binding = PageClientBinding.bind(view)
         initView()
         initToolBar()
         initObserver()
@@ -36,14 +28,6 @@ class ClientFragment : Fragment(R.layout.page_qiita) {
     }
 
     private fun initView() {
-        binding?.recyclerview?.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            val decoration = DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
-            addItemDecoration(decoration)
-            adapter = QiitaArticleListAdapter(onListItemClick = {_, _ ->
-                //Do nothing
-            })
-        }
     }
 
     private fun initToolBar() {
@@ -55,7 +39,7 @@ class ClientFragment : Fragment(R.layout.page_qiita) {
 //        }
 
         // タイトル
-        binding?.toolbar?.title = getString(R.string.qiita_title_recently)
+        binding?.toolbar?.title = getString(R.string.client_title)
 
         // NOTE: フラグメントが所有するアプリバーは onCreateOptionsMenu ではなく
         //       ここで onViewCreated 等で inflate する
@@ -63,54 +47,6 @@ class ClientFragment : Fragment(R.layout.page_qiita) {
     }
 
     private fun initObserver() {
-        viewModel.articles.observe(viewLifecycleOwner) {
-            (binding?.recyclerview?.adapter as QiitaArticleListAdapter)?.updateArticleList(it)
-            if (it.size > 0 && it[0].totalCount != null) {
-                Toast.makeText(
-                    requireActivity(),
-                    "${it[0].totalCount.toString()}件ヒットしました。",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-
-        viewModel.progressVisible.observe(viewLifecycleOwner) {
-            if (it) {
-                binding?.progressbar?.visibility = View.VISIBLE
-            } else {
-                binding?.progressbar?.visibility = View.GONE
-            }
-        }
-
-        viewModel.errorMessage.observe(viewLifecycleOwner) {
-            if (it != null) {
-                showSimpleErrorDialog(
-                    message = it,
-                    requestKey = TAG,
-                    requestBundle = bundleOf("errorMessage" to it),
-                    onClickCallback = { key, bundle ->
-                        if (key == TAG) {
-                            android.util.Log.d(
-                                "Hello",
-                                "Error dialog closed(${bundle.getString("errorMessage")})"
-                            )
-                            viewModel.clearError()
-                        }
-                    }
-                )
-            }
-        }
-
-        // 検索日でタイトルを更新
-        viewModel.searchDate.observe(viewLifecycleOwner) {
-            binding?.toolbar?.title = getString(R.string.qiita_title_posted_date) + convertToJapaneseCalender(it) + "〜"
-        }
-
-        // 検索画面から検索日を受け取る
-        findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<Bundle>("searchParam")
-            ?.observe(viewLifecycleOwner) {
-                viewModel.loadQiitaArticles(getDateFromBundle(it, "start"))
-            }
     }
 
     override fun onStart() {
--- kotlin-websocket/app/src/main/java/dev/seabat/android/kotlinwebsocket/ui/pages/server/ServerFragment.kt
+++ kotlin-websocket/app/src/main/java/dev/seabat/android/kotlinwebsocket/ui/pages/server/ServerFragment.kt
@@ -10,21 +10,21 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import dev.seabat.android.kotlinwebsocket.R
-import dev.seabat.android.kotlinwebsocket.databinding.PageTopBinding
+import dev.seabat.android.kotlinwebsocket.databinding.PageServerBinding
 import dev.seabat.android.kotlinwebsocket.ui.dialog.showSimpleErrorDialog
 
 @AndroidEntryPoint
-class ServerFragment : Fragment(R.layout.page_top) {
+class ServerFragment : Fragment(R.layout.page_server) {
     companion object {
         val TAG: String = ServerFragment::class.java.simpleName
     }
 
-    private var binding: PageTopBinding? = null
+    private var binding: PageServerBinding? = null
     private val viewModel: ServerViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = PageTopBinding.bind(view)
+        binding = PageServerBinding.bind(view)
         initView()
         initObserver()
         viewModel.loadRepositories()
@@ -32,64 +32,9 @@ class ServerFragment : Fragment(R.layout.page_top) {
     }
 
     private fun initView() {
-        binding?.recyclerview?.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            val decoration = DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
-            addItemDecoration(decoration)
-            adapter = RepositoryListAdapter(onListItemClick = {_, _ -> })
-        }
-
-        binding?.search?.setOnCloseListener {
-            binding?.search?.visibility = View.GONE
-            binding?.toolbar?.visibility = View.VISIBLE
-            true
-        }
-
-        binding?.search?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                viewModel.loadRepositories(query)
-                binding?.search?.visibility = View.GONE
-                binding?.toolbar?.visibility = View.VISIBLE
-                return false
-            }
-        })
     }
 
     private fun initObserver() {
-        viewModel.repositories.observe(viewLifecycleOwner) {
-            (binding?.recyclerview?.adapter as RepositoryListAdapter)?.updateRepositoryList(it)
-        }
-
-        viewModel.progressVisible.observe(viewLifecycleOwner) {
-            if (it) {
-                binding?.progressbar?.visibility = View.VISIBLE
-            } else {
-                binding?.progressbar?.visibility = View.GONE
-            }
-        }
-
-        viewModel.errorMessage.observe(viewLifecycleOwner) {
-            if (it != null) {
-                showSimpleErrorDialog(
-                    message = it,
-                    requestKey = TAG,
-                    requestBundle = bundleOf("errorMessage" to it),
-                    onClickCallback = { key, bundle ->
-                        if (key == TAG) {
-                            android.util.Log.d(
-                                "Hello",
-                                "Error dialog closed(${bundle.getString("errorMessage")})"
-                            )
-                            viewModel.clearError()
-                        }
-                    }
-                )
-            }
-        }
     }
 
     override fun onDestroyView() {
